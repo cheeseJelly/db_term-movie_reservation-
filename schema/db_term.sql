99999f8d8b1b7368f0051f1e096543a5a1fa2257
hanajitsu <hanajitsu94@gmail.com>
2017-06-25 05:59:13 +0900

    Add files via upload

--- schema/db_term.sql
+++ schema/db_term.sql
@@ -1,19 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.3
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- 호스트: localhost
--- 처리한 시간: 17-06-24 20:20 
--- 서버 버전: 5.1.41
--- PHP 버전: 5.2.12
+-- Host: localhost:3306
+-- 생성 시간: 17-06-24 13:55
+-- 서버 버전: 5.6.35
+-- PHP 버전: 5.6.30
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- 데이터베이스: `db_term`
@@ -25,12 +28,11 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- 테이블 구조 `booking_info`
 --
 
-CREATE TABLE IF NOT EXISTS `booking_info` (
-  `booking_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `booking_info` (
+  `booking_id` int(11) NOT NULL,
   `ticket_id` int(11) NOT NULL,
-  `user_no` int(11) NOT NULL,
-  PRIMARY KEY (`booking_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `user_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -38,11 +40,10 @@ CREATE TABLE IF NOT EXISTS `booking_info` (
 -- 테이블 구조 `genre`
 --
 
-CREATE TABLE IF NOT EXISTS `genre` (
-  `genre_id` int(11) NOT NULL AUTO_INCREMENT,
-  `genre_type` varchar(30) CHARACTER SET ucs2 NOT NULL,
-  PRIMARY KEY (`genre_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `genre_type` varchar(30) CHARACTER SET ucs2 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -50,14 +51,13 @@ CREATE TABLE IF NOT EXISTS `genre` (
 -- 테이블 구조 `movie`
 --
 
-CREATE TABLE IF NOT EXISTS `movie` (
-  `movie_no` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `movie` (
+  `movie_no` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `year` varchar(100) NOT NULL,
   `image_url` varchar(100) NOT NULL,
-  `playMinutes` int(11) NOT NULL,
-  PRIMARY KEY (`movie_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `playMinutes` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -65,12 +65,11 @@ CREATE TABLE IF NOT EXISTS `movie` (
 -- 테이블 구조 `movie_actor`
 --
 
-CREATE TABLE IF NOT EXISTS `movie_actor` (
-  `movie_actor_rel_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `movie_actor` (
+  `movie_actor_rel_id` int(11) NOT NULL,
   `movie_no` int(11) NOT NULL,
-  `people_id` int(11) NOT NULL,
-  PRIMARY KEY (`movie_actor_rel_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `people_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -78,12 +77,11 @@ CREATE TABLE IF NOT EXISTS `movie_actor` (
 -- 테이블 구조 `movie_director`
 --
 
-CREATE TABLE IF NOT EXISTS `movie_director` (
-  `movie_director_rel_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `movie_director` (
+  `movie_director_rel_id` int(11) NOT NULL,
   `movie_no` int(11) NOT NULL,
-  `people_id` int(11) NOT NULL,
-  PRIMARY KEY (`movie_director_rel_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `people_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -91,12 +89,11 @@ CREATE TABLE IF NOT EXISTS `movie_director` (
 -- 테이블 구조 `movie_genre`
 --
 
-CREATE TABLE IF NOT EXISTS `movie_genre` (
-  `movie_genre_rel_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `movie_genre` (
+  `movie_genre_rel_id` int(11) NOT NULL,
   `movie_no` int(11) NOT NULL,
-  `genre_id` int(11) NOT NULL,
-  PRIMARY KEY (`movie_genre_rel_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -104,11 +101,10 @@ CREATE TABLE IF NOT EXISTS `movie_genre` (
 -- 테이블 구조 `people`
 --
 
-CREATE TABLE IF NOT EXISTS `people` (
-  `people_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`people_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+CREATE TABLE `people` (
+  `people_id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -116,14 +112,25 @@ CREATE TABLE IF NOT EXISTS `people` (
 -- 테이블 구조 `review`
 --
 
-CREATE TABLE IF NOT EXISTS `review` (
-  `review_no` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `review` (
+  `review_no` int(11) NOT NULL,
   `user_no` int(11) NOT NULL,
-  `movie_no` int(11) NOT NULL,
+  `showTime_id` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`review_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `seat`
+--
+
+CREATE TABLE `seat` (
+  `seat_no` int(11) NOT NULL,
+  `theater_id` int(11) NOT NULL,
+  `id` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -131,15 +138,13 @@ CREATE TABLE IF NOT EXISTS `review` (
 -- 테이블 구조 `showtime`
 --
 
-CREATE TABLE IF NOT EXISTS `showtime` (
-  `showTime_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `showtime` (
+  `showTime_id` int(11) NOT NULL,
   `movie_no` int(11) NOT NULL,
   `theater_no` int(11) NOT NULL,
   `startTime` time NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  PRIMARY KEY (`showTime_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `show_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -147,13 +152,12 @@ CREATE TABLE IF NOT EXISTS `showtime` (
 -- 테이블 구조 `theater`
 --
 
-CREATE TABLE IF NOT EXISTS `theater` (
-  `theater_no` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `theater` (
+  `theater_no` int(11) NOT NULL,
   `theater_name` varchar(100) NOT NULL,
   `max_row` int(11) NOT NULL,
-  `max_col` int(11) NOT NULL,
-  PRIMARY KEY (`theater_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+  `max_col` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -161,14 +165,13 @@ CREATE TABLE IF NOT EXISTS `theater` (
 -- 테이블 구조 `ticket_info`
 --
 
-CREATE TABLE IF NOT EXISTS `ticket_info` (
-  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ticket_info` (
+  `ticket_id` int(11) NOT NULL,
   `showtime_id` int(11) NOT NULL,
   `seat_no` varchar(20) NOT NULL,
   `type_id` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY (`ticket_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -176,12 +179,11 @@ CREATE TABLE IF NOT EXISTS `ticket_info` (
 -- 테이블 구조 `ticket_type`
 --
 
-CREATE TABLE IF NOT EXISTS `ticket_type` (
-  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `ticket_type` (
+  `type_id` int(11) NOT NULL,
   `type` varchar(20) NOT NULL,
-  `discount` int(11) NOT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `discount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -189,12 +191,180 @@ CREATE TABLE IF NOT EXISTS `ticket_type` (
 -- 테이블 구조 `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` varchar(10) NOT NULL,
   `password` varchar(10) NOT NULL,
   `name` varchar(10) NOT NULL,
-  `user_no` int(11) NOT NULL AUTO_INCREMENT,
+  `user_no` int(11) NOT NULL,
   `is_admin` tinyint(1) NOT NULL,
-  `point` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `point` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `booking_info`
+--
+ALTER TABLE `booking_info`
+  ADD PRIMARY KEY (`booking_id`);
+
+--
+-- 테이블의 인덱스 `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- 테이블의 인덱스 `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`movie_no`);
+
+--
+-- 테이블의 인덱스 `movie_actor`
+--
+ALTER TABLE `movie_actor`
+  ADD PRIMARY KEY (`movie_actor_rel_id`);
+
+--
+-- 테이블의 인덱스 `movie_director`
+--
+ALTER TABLE `movie_director`
+  ADD PRIMARY KEY (`movie_director_rel_id`);
+
+--
+-- 테이블의 인덱스 `movie_genre`
+--
+ALTER TABLE `movie_genre`
+  ADD PRIMARY KEY (`movie_genre_rel_id`);
+
+--
+-- 테이블의 인덱스 `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`people_id`);
+
+--
+-- 테이블의 인덱스 `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`review_no`),
+  ADD KEY `review_no` (`review_no`);
+
+--
+-- 테이블의 인덱스 `seat`
+--
+ALTER TABLE `seat`
+  ADD PRIMARY KEY (`seat_no`);
+
+--
+-- 테이블의 인덱스 `showtime`
+--
+ALTER TABLE `showtime`
+  ADD PRIMARY KEY (`showTime_id`);
+
+--
+-- 테이블의 인덱스 `theater`
+--
+ALTER TABLE `theater`
+  ADD PRIMARY KEY (`theater_no`);
+
+--
+-- 테이블의 인덱스 `ticket_info`
+--
+ALTER TABLE `ticket_info`
+  ADD PRIMARY KEY (`ticket_id`),
+  ADD UNIQUE KEY `ticket_id` (`ticket_id`);
+
+--
+-- 테이블의 인덱스 `ticket_type`
+--
+ALTER TABLE `ticket_type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- 테이블의 인덱스 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_no`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `booking_info`
+--
+ALTER TABLE `booking_info`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- 테이블의 AUTO_INCREMENT `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- 테이블의 AUTO_INCREMENT `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `movie_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- 테이블의 AUTO_INCREMENT `movie_actor`
+--
+ALTER TABLE `movie_actor`
+  MODIFY `movie_actor_rel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- 테이블의 AUTO_INCREMENT `movie_director`
+--
+ALTER TABLE `movie_director`
+  MODIFY `movie_director_rel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- 테이블의 AUTO_INCREMENT `movie_genre`
+--
+ALTER TABLE `movie_genre`
+  MODIFY `movie_genre_rel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- 테이블의 AUTO_INCREMENT `people`
+--
+ALTER TABLE `people`
+  MODIFY `people_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- 테이블의 AUTO_INCREMENT `review`
+--
+ALTER TABLE `review`
+  MODIFY `review_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- 테이블의 AUTO_INCREMENT `seat`
+--
+ALTER TABLE `seat`
+  MODIFY `seat_no` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 테이블의 AUTO_INCREMENT `showtime`
+--
+ALTER TABLE `showtime`
+  MODIFY `showTime_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- 테이블의 AUTO_INCREMENT `theater`
+--
+ALTER TABLE `theater`
+  MODIFY `theater_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- 테이블의 AUTO_INCREMENT `ticket_info`
+--
+ALTER TABLE `ticket_info`
+  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+--
+-- 테이블의 AUTO_INCREMENT `ticket_type`
+--
+ALTER TABLE `ticket_type`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- 테이블의 AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
